d53bea341ed98d6b421c19ecb2676260b4cc251d
Shobhit Vishwakarma <v.shobhit@benthonlabs.com>
2023-02-02 13:25:17 +0530

    Unsaved

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://www.benthonlabs.com/images/benthon-logo.png" className="App-logo" alt="logo" />
+        <p>
+          This is a testing project
+        </p>
+        <a
+          className="App-link"
+          href="https://www.benthonlabs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Benthon labs
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
